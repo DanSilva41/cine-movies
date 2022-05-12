7ba8e59fb70bbcb9923cc7ec86a8ee2bc2536bbe
Danilo Silva <danilosilvap@outlook.com.br>
2022-05-12 09:32:03 -0300

    feat: get movies from api with axios

---
+++ src/api/MoviesService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('/movie/popular'))
+    }
+}
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,5 +1,5 @@
 export const MovieItem = ({ title }) => (
-    <li>
+    <li key={title}>
         {title}
     </li>
 )
